e4efa500aa9dd3efbebc9ed6a9e632c4d5ad89fe
Yuva Raja <iampanneeryuvaraja@gmail.com>
2024-10-12 23:10:07 +0100

    CI Test

---
+++ GCP-CLI/gcloud.sh
@@ -0,0 +1,5 @@
+gcloud projects create flask-argocd-experiment
+gcloud components update 
+    #here CLI promts and need to give 'Y' key
+gcloud projects describe flask-argocd-experiment
+    # here CLI drops Project name and ID that are needed as project_name and project_ID variables for further use
